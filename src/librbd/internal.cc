57d66ae1ab3d7c74f7d784b04481fe42f6c68ce5
Jason Dillaman <dillaman@redhat.com>
2015-03-25 21:09:47 -0400

    librbd: ensure fast diff feature relies on object map
    Throw an error if the fast diff feature is enabled w/o the
    object map -- or if the object map is disabled while fast
    diff is still enabled.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/internal.cc
+++ src/librbd/internal.cc
@@ -952,7 +952,11 @@ reprotect_and_return_err:
       }
     }
 
-    if ((features & RBD_FEATURE_OBJECT_MAP) != 0) {
+    if ((features & RBD_FEATURE_FAST_DIFF) != 0 &&
+        (features & RBD_FEATURE_OBJECT_MAP) == 0) {
+      lderr(cct) << "cannot use fast diff without object map" << dendl;
+      goto err_remove_header;
+    } else if ((features & RBD_FEATURE_OBJECT_MAP) != 0) {
       if ((features & RBD_FEATURE_EXCLUSIVE_LOCK) == 0) {
         lderr(cct) << "cannot use object map without exclusive lock" << dendl;
         goto err_remove_header;
@@ -1444,6 +1448,12 @@ reprotect_and_return_err:
           return -EINVAL;
         }
         mask |= RBD_FEATURE_EXCLUSIVE_LOCK;
+      } else if (feature == RBD_FEATURE_FAST_DIFF) {
+        if ((ictx->features & RBD_FEATURE_OBJECT_MAP) == 0) {
+          lderr(cct) << "cannot enable fast diff" << dendl;
+          return -EINVAL;
+        }
+        mask |= (RBD_FEATURE_OBJECT_MAP | RBD_FEATURE_EXCLUSIVE_LOCK);
       }
     } else {
       if (feature == RBD_FEATURE_EXCLUSIVE_LOCK) {
@@ -1453,6 +1463,10 @@ reprotect_and_return_err:
         }
         mask |= RBD_FEATURE_OBJECT_MAP;
       } else if (feature == RBD_FEATURE_OBJECT_MAP) {
+        if ((ictx->features & RBD_FEATURE_FAST_DIFF) != 0) {
+          lderr(cct) << "cannot disable object map" << dendl;
+          return -EINVAL;
+        }
         r = remove_object_map(ictx);
         if (r < 0) {
           lderr(cct) << "failed to remove object map" << dendl;
