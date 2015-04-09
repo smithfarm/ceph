c0d99df90869b35c04b040fc36dfc3712389e16e
Jason Dillaman <dillaman@redhat.com>
2015-04-09 13:33:09 -0400

    librbd: AioRequest::send no longer returns a result
    The librados calls used by AioRequest::send should always return
    zero unless there is a bug.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/internal.cc
+++ src/librbd/internal.cc
@@ -3477,8 +3477,7 @@ reprotect_and_return_err:
 	c->add_request();
 
 	req->set_op_flags(op_flags);
-	r = req->send();
-        assert(r == 0);
+	req->send();
       }
     }
 
@@ -3617,8 +3616,7 @@ reprotect_and_return_err:
 			  snapc, req_comp);
       }
 
-      r = req->send();
-      assert(r == 0);
+      req->send();
     }
 
     if (ictx->object_cacher) {
@@ -3784,8 +3782,7 @@ reprotect_and_return_err:
 				    q->length, q->offset,
 				    cache_comp, op_flags);
 	} else {
-	  r = req->send();
-          assert(r == 0);
+	  req->send();
 	}
       }
     }
