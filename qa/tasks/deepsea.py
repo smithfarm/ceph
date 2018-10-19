467d0f3bc8e1a5f7881ca0315249ae662b9f331b
Nathan Cutler <ncutler@suse.com>
2018-10-19 16:11:10 +0200

    qa/deepsea: add cluster deploy capability to deepsea task
    With this commit, workunit is no longer necessary because
    the deepsea task itself can deploy the cluster (by running
    health-ok.sh and/or other commands).
    
    This commit also ports the initialization_sequence function from
    health-ok.sh into the deepsea task.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- qa/tasks/deepsea.py
+++ qa/tasks/deepsea.py
@@ -9,9 +9,12 @@ from salt_manager import SaltManager
 from teuthology import misc
 from teuthology.exceptions import (CommandFailedError, ConfigError)
 from teuthology.orchestra import run
+from teuthology.misc import sh
 from teuthology.task import Task
+from util import check_config_key, copy_directory_recursively, get_remote_for_role
 
 log = logging.getLogger(__name__)
+health_ok_cmd = "health-ok.sh --teuthology"
 
 class DeepSea(Task):
     """
@@ -26,6 +29,12 @@ class DeepSea(Task):
         install:
             package|pkg deepsea will be installed via package system
             source|src  deepsea will be installed via 'make install' (default)
+        cli:
+            true        deepsea CLI will be used (default)
+            false       deepsea CLI will not be used
+        deploy: (whether to deploy Ceph; defaults to False)
+            health-ok:
+            - list of commands to run as root
 
     Example:
 
@@ -38,21 +47,29 @@ class DeepSea(Task):
     :param ctx: the argparse.Namespace object
     :param config: the config dict
     """
+
     def __init__(self, ctx, config):
         super(DeepSea, self).__init__(ctx, config)
-
-        def _check_config_key(key, default_value):
-            if key not in self.config or not self.config[key]:
-                self.config[key] = default_value
-
-        _check_config_key('install', 'source')
-        _check_config_key('repo', 'https://github.com/SUSE/DeepSea.git')
-        _check_config_key('branch', 'master')
-
-        log.debug("Munged config is {}".format(self.config))
-
+        check_config_key(self.config, 'repo', 'https://github.com/SUSE/DeepSea.git')
+        check_config_key(self.config, 'branch', 'master')
+        check_config_key(self.config, 'install', 'source')
+        check_config_key(self.config, 'cli', True)
+        self.health_ok_cmd = health_ok_cmd
+        if self.config['cli']:
+            self.health_ok_cmd += ' --cli'
+        check_config_key(self.config, 'deploy', None)
         self.sm = SaltManager(self.ctx, self.config)
         self.master_remote = self.sm.master_remote
+        self.log.debug("ctx.config {}".format(ctx.config))
+        self.roles = ctx.config['roles']
+        self._introspect_nodes()
+        if self.config['deploy']:
+            if not isinstance(self.config['deploy'], dict):
+                raise ConfigError("deepsea task: deploy config param takes a dict")
+            check_config_key(self.config['deploy'], '_init_seq', True)
+            # FIXME: set _dev_env based on number of cluster nodes (self.cluster_nodes)
+            check_config_key(self.config['deploy'], '_dev_env', False)
+        log.debug("Munged config is {}".format(self.config))
 
     def __purge_osds(self):
         # FIXME: purge osds only on nodes that have osd role
@@ -76,7 +93,76 @@ class DeepSea(Task):
                 'for f in vdb2 vdc2 vdd2 vde2 vdf2; do test -b /dev/$f && umount /dev/$f || true ; done'
                 ])
 
+    def _master_whoami(self):
+        """Demonstrate that remote.run() does not run stuff as root"""
+        self.master_remote.run(args=[
+            'whoami',
+        ])
+
+    def _master_rpm_q(self, pkg_name):
+        """Run rpm -q on the Salt Master node"""
+        # FIXME: should possibly take a list of pkg_names
+        installed = True
+        try:
+            self.master_remote.run(args=[
+                'rpm', '-q', pkg_name
+            ])
+        except CommandFailedError:
+            installed = False
+        return installed
+
+    def _master_python_version(self, py_version):
+        """
+        Determine which python version(s) are installed on the Salt Master node.
+        Bail out if Python 3 not installed.
+        """
+        python_binary = 'python{}'.format(py_version)
+        installed = True
+        try:
+            self.master_remote.run(args=[
+                'type',
+                python_binary,
+                run.Raw('>'),
+                '/dev/null',
+                run.Raw('2>&1'),
+            ])
+        except CommandFailedError:
+            installed = False
+        if installed:
+            self.master_remote.run(args=[
+                python_binary,
+                '--version'
+            ])
+        else:
+            self.log.info('{} not installed on master node'.format(python_binary))
+        return installed
+
+    def _deepsea_cli_version(self):
+        """Use DeepSea CLI to display the DeepSea version under test"""
+        installed = True
+        try:
+            self.master_remote.run(args=[
+                'type',
+                'deepsea',
+                run.Raw('>'),
+                '/dev/null',
+                run.Raw('2>&1'),
+            ])
+        except CommandFailedError:
+            installed = False
+        if installed:
+            self.master_remote.run(args=[
+                'deepsea',
+                '--version',
+            ])
+        else:
+            self.log.info("deepsea CLI not installed")
+
     def __install_deepsea_from_source(self):
+        """Install DeepSea from source (unless already installed from RPM)"""
+        if self._master_rpm_q('deepsea'):
+            self.log.info("DeepSea already installed from RPM")
+            return None
         self.log.info("Installing DeepSea from source - repo: {}, branch: {}"
                       .format(self.config["repo"], self.config["branch"]))
         self.master_remote.run(args=[
@@ -128,52 +214,161 @@ class DeepSea(Task):
             ])
 
     def __install_deepsea_using_zypper(self):
-            self.log.info("Installing DeepSea using zypper")
-            self.master_remote.run(args=[
-                'sudo',
-                'zypper',
-                '--non-interactive',
-                'search',
-                '--details',
-                'deepsea'
-                ])
-            self.master_remote.run(args=[
-                'sudo',
-                'zypper',
-                '--non-interactive',
-                '--no-gpg-checks',
-                'install',
-                '--force',
-                '--no-recommends',
-                'deepsea',
-                'deepsea-cli',
-                'deepsea-qa'
-                ])
-
-    def setup(self):
-        super(DeepSea, self).setup()
-        log.debug("beginning of DeepSea task setup method...")
-        pass
-        log.debug("end of DeepSea task setup...")
-
-    def begin(self):
-        super(DeepSea, self).begin()
-        log.debug("beginning of DeepSea task begin method...")
+        """Install DeepSea using zypper"""
+        self.log.info("Installing DeepSea using zypper")
         self.master_remote.run(args=[
             'sudo',
-            'rpm',
-            '-q',
-            'ceph-test'
+            'zypper',
+            '--non-interactive',
+            'search',
+            '--details',
+            'deepsea'
             ])
-        suite_path = self.ctx.config.get('suite_path')
-        log.info("suite_path is ->{}<-".format(suite_path))
+        self.master_remote.run(args=[
+            'sudo',
+            'zypper',
+            '--non-interactive',
+            '--no-gpg-checks',
+            'install',
+            '--force',
+            '--no-recommends',
+            'deepsea',
+            'deepsea-cli',
+            'deepsea-qa'
+            ])
+
+    def _set_pillar_deepsea_minions(self):
+        """Set deepsea_minions pillar value"""
+        self.master_remote.run(args=[
+            'sudo',
+            'sh',
+            '-c',
+            'echo "deepsea_minions: \'*\'" > /srv/pillar/ceph/deepsea_minions.sls',
+            run.Raw(';'),
+            'cat',
+            '/srv/pillar/ceph/deepsea_minions.sls',
+        ])
+
+    def _initialization_sequence(self):
+        """
+        Port of initialization_sequence from health-ok.sh
+        """
+        self._master_rpm_q('ceph')
+        self._master_rpm_q('ceph-test')
+        self._master_rpm_q('salt-master')
+        self._master_rpm_q('salt-minion')
+        self._master_rpm_q('salt-api')
+        self._master_python_version(2)
+        if not self._master_python_version(3):
+            raise ConfigError("Python 3 not installed on master node - bailing out!")
+        if self.config['cli']:
+            self._deepsea_cli_version()
+        self._set_pillar_deepsea_minions()
+        self.sm.sync_pillar_data()
+        self.sm.cat_salt_master_conf()
+        self.sm.cat_salt_minion_confs()
+
+    def _introspect_nodes(self):
+        """
+        Sets self.role_nodes, self.salt_nodes, self.cluster_nodes,
+        self.client_nodes. Tthe latter are understood to be client-ONLY nodes,
+        and:
+        self.role_nodes == self.salt_nodes == (self.cluster_nodes + self.client_nodes)
+        """
+        self.role_nodes = len(self.roles)
+        self.salt_nodes = self.sm.ping_minions()
+        if self.role_nodes == self.salt_nodes:
+            self.log.info("Role nodes ({}) is equal to Salt nodes ({})"
+                          .format(self.role_nodes, self.salt_nodes))
+        else:
+            raise ConfigError("deepsea task: Role nodes ({}) is NOT equal to Salt nodes ({})"
+                              .format(self.role_nodes, self.salt_nodes))
+        # FIXME: set self.cluster_nodes
+        # FIXME: set self.client_nodes
+        log.debug("role_nodes {}, salt_nodes {}"
+                  .format(self.role_nodes, self.salt_nodes))
+
+    def _install_deepsea(self):
+        """
+        Install DeepSea on master node
+        """
         if self.config['install'] in ['source', 'src']:
             self.__install_deepsea_from_source()
         elif self.config['install'] in ['package', 'pkg']:
             self.__install_deepsea_using_zypper()
         else:
-            raise ConfigError("Unsupported deepsea install method '%s'"
-                                                % self.config['install'])
+            raise ConfigError("Unsupported deepsea install method ->{}<-"
+                              .format(self.config['install']))
+
+    def _copy_health_ok(self):
+        """
+        Copy health-ok.sh from teuthology VM to master_remote
+        """
+        suite_path = self.ctx.config.get('suite_path')
+        log.info("suite_path is ->{}<-".format(suite_path))
+        sh("ls -l {}".format(suite_path))
+        health_ok_path = suite_path + "/deepsea/health-ok"
+        sh("test -d " + health_ok_path)
+        copy_directory_recursively(health_ok_path, self.master_remote, "health-ok")
+        self.master_remote.run(args=[
+            "pwd",
+            run.Raw(";"),
+            "ls",
+            "-lR",
+            "health-ok",
+            ])
+
+    def _check_config_deploy_health_ok(self, deploy):
+        check_config_key(deploy, 'health-ok', None)
+        deploy_cmdlist = deploy['health-ok']
+        if deploy_cmdlist == None:
+            deploy_cmdlist = [self.health_ok_cmd]
+        if not isinstance(deploy_cmdlist, list):
+            raise ConfigError("deepsea task: health-ok config param takes a list")
+        if not deploy_cmdlist:
+            raise ConfigError("deepsea task: health-ok command list must not be empty")
+        self.log.info("deepsea task: deployment command list: {}"
+                       .format(deploy_cmdlist))
+        deploy['health-ok'] = deploy_cmdlist
+        log.debug("Munged config is {}".format(self.config))
+ 
+    def _run_health_ok(self, deploy):
+        for cmd in deploy['health-ok']:
+            self.master_remote.run(args=[
+                'cd',
+                'health-ok',
+                run.Raw(';'),
+                'sudo',
+                'bash',
+                '-c',
+                cmd,
+                ])
+
+    def _maybe_deploy_ceph(self):
+        deploy = self.config.get('deploy')
+        if deploy:
+            self._check_config_deploy_health_ok(deploy)
+            log.debug("Munged config is {}".format(self.config))
+            if deploy['_init_seq']:
+                self._initialization_sequence()
+            else:
+                self.log.warn("deepsea task: skipping initialization sequence")
+            self._copy_health_ok()
+            self._run_health_ok(deploy)
+        else:
+            self.log.info("deepsea task: deploy config param not given; not deploying Ceph")
+
+    def setup(self):
+        super(DeepSea, self).setup()
+        log.debug("beginning of DeepSea task setup method...")
+        self._install_deepsea()
+        log.debug("end of DeepSea task setup...")
+
+    def begin(self):
+        super(DeepSea, self).begin()
+        log.debug("beginning of DeepSea task begin method...")
+        self._master_whoami()
+        self._maybe_deploy_ceph()
         log.debug("end of DeepSea task begin method...")
 
     def end(self):
