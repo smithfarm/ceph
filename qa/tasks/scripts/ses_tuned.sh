ff5ac8af9602dbfb7e08ab9963c1a8b3c5987a94
Denis Polom <dpolom@suse.de>
2019-11-30 13:42:39 +0100

    fixed

--- qa/tasks/scripts/ses_tuned.sh
+++ qa/tasks/scripts/ses_tuned.sh
@@ -10,21 +10,21 @@ function check_tuned() {
             local role=$(salt $server cmd.run "tuned-adm active | egrep '$role|virtual-guest'" --output=json | jq -r .[] | cut -d : -f 2)
       done
     fi
-    salt -L ${minions// /,} cmd.run "tuned-adm active | egrep '$role|virtual-guest'" 
+    salt -L "${minions// /,}" cmd.run "tuned-adm active | egrep '$role|virtual-guest'" 
 }
 
 
 role_list=($(salt '*' pillar.get "roles" | sort -u | grep "\- " | awk '{print $2}' | egrep -v 'admin|master|grafana'))
 
-for role in $role_list
+for role in ${role_list[@]}
 do 
-    [ $role == ceph-osd ] && role=storage
-    minions=($(salt-run select.minions roles=$role | grep -v $(hostname -f)))
+    test "$role" == "ceph-osd" && role=storage
+    minions=($(salt-run select.minions roles=$role --output=json | jq -r .[] | grep -v $(hostname -f)))
     minion_list="${minions[@]}"  # this converts array to space separated list
-    minion_list=${minion// /,}       # this replaces spaces with commas, likely we do not have spaces in hostnames
-    if [ -n $minion_list] ; then
+#    minion_list=${minion// /,}       # this replaces spaces with commas, likely we do not have spaces in hostnames
+    if [ -n "$minion_list" ] ; then
         check_tuned $role ${minions[@]}
-        salt -L $minion_list service.restart tuned.service
+        salt -L "$minion_list" service.restart tuned.service
         check_tuned $role ${minions[@]}
     fi
 done
