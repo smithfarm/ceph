1387ceaf78464518975cb8efb2da92b07f7a4c9c
Nathan Cutler <ncutler@suse.com>
2020-01-15 10:04:32 +0100

    Revert "rgw_file: introduce fast S3 Unix stats (immutable)"
    This reverts commit 62139b60fa8b2f26f1199c790aac22bb03575fa0.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -272,9 +272,6 @@ namespace rgw {
 #define CREATE_FLAGS(x) \
     ((x) & ~(RGWFileHandle::FLAG_CREATE|RGWFileHandle::FLAG_LOCK))
 
-    static constexpr uint32_t RCB_MASK = \
-      RGW_SETATTR_MTIME|RGW_SETATTR_CTIME|RGW_SETATTR_ATIME|RGW_SETATTR_SIZE;
-
     friend class RGWLibFS;
 
   private:
@@ -639,14 +636,10 @@ namespace rgw {
       state.size = size;
     }
 
-    void set_times(const struct timespec &ts) {
-      state.ctime = ts;
+    void set_times(real_time t) {
+      state.ctime = real_clock::to_timespec(t);
       state.mtime = state.ctime;
       state.atime = state.ctime;
-    }
-
-    void set_times(real_time t) {
-      set_times(real_clock::to_timespec(t));
     }
 
     void set_ctime(const struct timespec &ts) {
@@ -1180,11 +1173,6 @@ namespace rgw {
 			       RGWLibFS::BucketStats& bs,
 			       uint32_t flags);
 
-    LookupFHResult fake_leaf(RGWFileHandle* parent, const char *path,
-			     enum rgw_fh_type type = RGW_FS_TYPE_NIL,
-			     struct stat *st = nullptr, uint32_t mask = 0,
-			     uint32_t flags = RGWFileHandle::FLAG_NONE);
-
     LookupFHResult stat_leaf(RGWFileHandle* parent, const char *path,
 			     enum rgw_fh_type type = RGW_FS_TYPE_NIL,
 			     uint32_t flags = RGWFileHandle::FLAG_NONE);
@@ -1411,7 +1399,7 @@ public:
     rgw_fh->add_marker(off, rgw_obj_key{marker.data(), ""},
 		       RGW_FS_TYPE_DIRECTORY);
     ++d_count;
-    return rcb(name.data(), cb_arg, off, nullptr, 0, RGW_LOOKUP_FLAG_DIR);
+    return rcb(name.data(), cb_arg, off, RGW_LOOKUP_FLAG_DIR);
   }
 
   bool eof() {
@@ -1515,7 +1503,7 @@ public:
   }
 
   int operator()(const boost::string_ref name, const rgw_obj_key& marker,
-		 const ceph::real_time& t, const uint64_t fsz, uint8_t type) {
+		uint8_t type) {
 
     assert(name.length() > 0); // all cases handled in callers
 
@@ -1524,25 +1512,10 @@ public:
     if (unlikely(!! ioff)) {
       *ioff = off;
     }
-
     /* update traversal cache */
     rgw_fh->add_marker(off, marker, type);
     ++d_count;
-
-    /* set c/mtime and size from bucket index entry */
-    struct stat st = {};
-#ifdef HAVE_STAT_ST_MTIMESPEC_TV_NSEC
-    st.st_atimespec = ceph::real_clock::to_timespec(t);
-    st.st_mtimespec = st.st_atimespec;
-    st.st_ctimespec = st.st_atimespec;
-#else
-    st.st_atim = ceph::real_clock::to_timespec(t);
-    st.st_mtim = st.st_atim;
-    st.st_ctim = st.st_atim;
-#endif
-    st.st_size = fsz;
-
-    return rcb(name.data(), cb_arg, off, &st, RGWFileHandle::RCB_MASK,
+    return rcb(name.data(), cb_arg, off,
 	       (type == RGW_FS_TYPE_DIRECTORY) ?
 	       RGW_LOOKUP_FLAG_DIR :
 	       RGW_LOOKUP_FLAG_FILE);
@@ -1576,13 +1549,9 @@ public:
 			     << " prefix=" << prefix << " "
 			     << " obj path=" << iter.key.name
 			     << " (" << sref << ")" << ""
-			     << " mtime="
-			     << real_clock::to_time_t(iter.meta.mtime)
-			     << " size=" << iter.meta.accounted_size
 			     << dendl;
 
-      if (! this->operator()(sref, next_marker, iter.meta.mtime,
-			     iter.meta.accounted_size, RGW_FS_TYPE_FILE)) {
+      if (! this->operator()(sref, next_marker, RGW_FS_TYPE_FILE)) {
 	/* caller cannot accept more */
 	lsubdout(cct, rgw, 5) << "readdir rcb failed"
 			      << " dirent=" << sref.data()
@@ -1593,8 +1562,6 @@ public:
       }
       ++ix;
     }
-
-    auto cnow = real_clock::now();
     for (auto& iter : common_prefixes) {
 
       lsubdout(cct, rgw, 15) << "readdir common prefixes prefix: " << prefix
@@ -1631,8 +1598,7 @@ public:
 	return;
       }
 
-      if (! this->operator()(sref, next_marker, cnow, 0,
-			     RGW_FS_TYPE_DIRECTORY)) {
+      if (! this->operator()(sref, next_marker, RGW_FS_TYPE_DIRECTORY)) {
 	/* caller cannot accept more */
 	lsubdout(cct, rgw, 5) << "readdir rcb failed"
 			      << " dirent=" << sref.data()
